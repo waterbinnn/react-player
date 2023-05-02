1bb03726c521696880685a718c5b0a7a0fc4f879
Subin Lee <ewaterbinn@gmail.com>
2023-05-02 20:25:32 +0900

    feat: main page 리스트 개발

--- src/containers/main/Main.tsx
+++ src/containers/main/Main.tsx
@@ -1,22 +1,29 @@
 import { ShortsList } from "./shortsList";
 import classNames from "classnames/bind";
 import styles from "./Main.module.scss";
+import { DataType } from "@/contents/data";
 
 const cx = classNames.bind(styles);
 
-export const Main = () => {
+interface MainProps {
+  popularList: DataType[];
+  hotList: DataType[];
+}
+
+export const Main = ({ popularList, hotList }: MainProps) => {
   return (
     <>
       <div className={cx("container")}>
+        <h1 className={cx("title")}>🎬</h1>
         <section>
           <h2 className="visually-hidden">main</h2>
-          <section>
-            <h2>요즘 뜨는</h2>
-            <ShortsList />
+          <section className={cx("list-wrap")}>
+            <h2>⚡️ 요즘 뜨는</h2>
+            <ShortsList list={popularList} />
           </section>
-          <section>
-            <h2>지금 인기 있는</h2>
-            <ShortsList />
+          <section className={cx("list-wrap")}>
+            <h2>🔥 지금 인기 있는</h2>
+            <ShortsList list={hotList} />
           </section>
         </section>
       </div>
--- src/containers/main/shortsList/ShortsList.tsx
+++ src/containers/main/shortsList/ShortsList.tsx
@@ -1,24 +1,39 @@
-import { DataType, hotList, popularList } from "@/contents/data";
+import { DataType } from "@/contents/data";
 import React, { useEffect, useState } from "react";
 import classNames from "classnames/bind";
 import styles from "./ShortsList.module.scss";
+import { useRouter } from "next/router";
+import path from "path";
 
 const cx = classNames.bind(styles);
 
-export const ShortsList = () => {
+interface ShortsListProps {
+  list: DataType[];
+}
+
+export const ShortsList = (list: ShortsListProps) => {
+  const router = useRouter();
+
+  const handleMoveToUrl = (url: string) => {
+    router.push(`shorts`);
+  };
+
   return (
     <>
       <ul className={cx("player-wrap")}>
-        {hotList.map((item: DataType) => (
-          <li key={item.id} className={cx("list")}>
-            {/* 클릭시 링크이동 */}
+        {list.list.map((item: DataType) => (
+          <li
+            key={item.id}
+            className={cx("list")}
+            onClick={() => handleMoveToUrl(item.url)}
+          >
             <div
               style={{
                 backgroundImage: `url("${item.image}")`,
               }}
               className={cx("thumbnail")}
             ></div>
-            <strong>{item.title}</strong>
+            <strong className={cx("title")}>{item.title}</strong>
           </li>
         ))}
       </ul>
